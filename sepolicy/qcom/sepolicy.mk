BlissRoms/platform_vendor_bliss

sepolicy: Fix path for QCOM Sepolicy
Signed-off-by: Meninblack007 <sanyam.53jain@gmail.com>

sepolicy/qcom/sepolicy.mk
@@ -1,2 +1,2 @@
 BOARD_SEPOLICY_DIRS += \
-    vendor/cm/sepolicy/qcom
+    vendor/aosp/sepolicy/qcom

QuantumDroid/platform_vendor_qd

Signed-off-by: Hriday Sharma <hridaysharma42@gmail.com>

sepolicy/qcom/sepolicy.mk
@@ -1,2 +1,2 @@
 BOARD_SEPOLICY_DIRS += \
-    vendor/cm/sepolicy/qcom
+    vendor/qd/sepolicy/qcom